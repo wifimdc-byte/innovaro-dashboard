29faf9e73772b08a5a3921ff797573872b02820d
dfa2307 <dan.franciscoo@gmail.com>
2026-07-17 16:30:41 -0300

    Adicionando main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import { DashboardProvider } from "./context/DashboardContext";
 
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <DashboardProvider>
-            <App />
-        </DashboardProvider>
+        <App />
     </React.StrictMode>
 );
